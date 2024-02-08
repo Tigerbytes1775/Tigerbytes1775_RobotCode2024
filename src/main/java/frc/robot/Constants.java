006a16231b65716d8ab58f921b3f71f21d299b9f
Tigerbytes 1775 Team Account <tigerbytesprogramming@gmail.com>
2024-02-08 16:21:18 -0600

    RollerAttempt

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -16,56 +16,8 @@ package frc.robot;
 public final class Constants {
   public static class OperatorConstants {
     public static final int kDriverControllerPort = 0;
-  }/* 
-//--------------------------------------------------------------------------------
-  // Locations for the swerve drive modules relative to the robot center.
-Translation2d m_frontLeftLocation = new Translation2d(0.381, 0.381);
-Translation2d m_frontRightLocation = new Translation2d(0.381, -0.381);
-Translation2d m_backLeftLocation = new Translation2d(-0.381, 0.381);
-Translation2d m_backRightLocation = new Translation2d(-0.381, -0.381);
-
-// Creating my kinematics object using the module locations
-SwerveDriveKinematics m_kinematics = new SwerveDriveKinematics(
-  m_frontLeftLocation, m_frontRightLocation, m_backLeftLocation, m_backRightLocation
-);
-//----------------------------------------------------------------------------------
-// Example chassis speeds: 1 meter per second forward, 3 meters
-// per second to the left, and rotation at 1.5 radians per second
-// counterclockwise.
-ChassisSpeeds speeds = new ChassisSpeeds(1.0, 3.0, 1.5);
-
-// Convert to module states
-SwerveModuleState[] moduleStates = m_kinematics.toSwerveModuleStates(speeds);
-
-// Front left module state
-SwerveModuleState frontLeft = moduleStates[0];
-
-// Front right module state
-SwerveModuleState frontRight = moduleStates[1];
-
-// Back left module state
-SwerveModuleState backLeft = moduleStates[2];
-
-// Back right module state
-SwerveModuleState backRight = moduleStates[3];
-//-------------------------------------------------------------------------------------------
-public class Example {
-  private final StructArrayPublisher<SwerveModuleState> publisher;
-
-  public Example() {
-    // Start publishing an array of module states with the "/SwerveStates" key
-    publisher = NetworkTableInstance.getDefault()
-      .getStructArrayTopic("/SwerveStates", SwerveModuleState.struct).publish();
+    
   }
+  
 
-  public void periodic() {
-    // Periodically send a set of module states
-    publisher.set(new SwerveModuleState[] {
-      frontLeft,
-      frontRight,
-      backLeft,
-      backRight
-    });
-  }
-}*/
 }
